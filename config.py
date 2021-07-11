2690a9f537a252f441bfa63e706ad017dfa75264
Meridian-Onset <jma64wright@gmail.com>
2021-07-11 14:23:34 +0200

    Updated config file with plotting info

--- config.py
+++ config.py
@@ -2,3 +2,14 @@ seed = 420 # dank
 
 field_size = 100
 
+actorPlottingConfig = {
+    'color' : '#3BEBA8',
+    'marker' : 'o',
+    'label' : 'actors'
+}
+
+foodPlottingConfig = {
+    'color' : 'red',
+    'marker' : 'x',
+    'label' : 'food'
+}
