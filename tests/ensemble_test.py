17e44a0455c16b8b955c21cbc76382a3b51c4afb
Meridian-Onset <jma64wright@gmail.com>
2021-09-17 11:33:38 +0200

    :hammer: Added tests that verify complete functionality of basic machinery

--- tests/ensemble_test.py
+++ tests/ensemble_test.py
@@ -4,14 +4,13 @@ from src.sim_components.Actors import Basic_Actor
 def test_initialisation():
     # Assert that ensemble initialises correctly and runs all different vanilla methods
     ensemble_for_testing = Ensemble(Basic_Actor, 100)
-    assert ensemble_for_testing is Ensemble
+    assert isinstance(ensemble_for_testing, Ensemble)
 
     # Test array method
     ensemble_arrays = ensemble_for_testing.toArray('actors')
-    assert ensemble_arrays.length >= 2
-    assert ensemble_arrays[0].length == 100
-    assert ensemble_arrays[1].length == 100
+    assert len(ensemble_arrays) >= 2
+    assert len(ensemble_arrays[0]) == 100
+    assert len(ensemble_arrays[1]) == 100
 
     ensemble_for_testing.start(100)
     assert ensemble_for_testing is Ensemble
-    
