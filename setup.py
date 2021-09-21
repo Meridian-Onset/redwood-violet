33a762ae445fc0927e238666a46d284c45aca582
Meridian-Onset <jma64wright@gmail.com>
2021-09-21 19:35:24 +0200

    :bug: Explicity included json files in the data_packages argument. Previously the lack of said file would cause tox py39 test to fail with a FileNotFound Error

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup, find_packages
 
 if __name__ == '__main__':
     setup(
-        package_dir = {"" : "src"},
-        packages = find_packages(where='src')
+        package_dir={"" : "src"},
+        packages=find_packages(where='src'),
+        package_data={
+            "": ["*.json"]
+        }
     )
